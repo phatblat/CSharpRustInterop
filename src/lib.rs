187afc10df8ebaeb7432bdc912af2bdf631990a6
Ben Chatelain <ben@octop.ad>
2022-03-19 09:52:53 -0600

    ✨Add add_numbers external function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+#[no_mangle]
+pub extern fn add_numbers(number1: i32, number2: i32) -> i32 {
+    println!("Hello from rust!");
+    number1 + number2
 }
