159d16a61b29e4445d1b232cd562238812f749f7
Ben Chatelain <ben@octop.ad>
2022-03-20 08:49:04 -0600

    ✨Add call to extern function

--- rust_dll_poc/Program.cs
+++ rust_dll_poc/Program.cs
@@ -1,3 +1,15 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.InteropServices;
 
+
+public class RustTest
+{
+    [DllImport("cs_call_rst")]
+    private unsafe static extern Int32 add_numbers(Int32 number1, Int32 number2);
+
+    static void Main(string[] args)
+    {
+        var addedNumbers = add_numbers(10, 5);
+        Console.WriteLine(addedNumbers);
+        Console.ReadLine();
+    }
+}
