ea7de917614170b1ddc5df7f04fde8d8e3556bde
Ben Chatelain <ben@octop.ad>
2022-03-20 20:07:42 -0600

    ✨ Add API returning a struct

--- csharp/rust_dll_poc/Program.cs
+++ csharp/rust_dll_poc/Program.cs
@@ -2,10 +2,20 @@ using System.Runtime.InteropServices;
 
 namespace rust_dll_poc;
 
+[StructLayout(LayoutKind.Sequential)]
+public struct SampleStruct
+{
+    public Int16 field_one;
+    public Int32 field_two;
+}
+
 public static class RustTest
 {
     [DllImport("cs_call_rst", EntryPoint = "add_numbers", CallingConvention = CallingConvention.Cdecl)]
     private static extern Int32 add_numbers(Int32 number1, Int32 number2);
+
+    [DllImport("cs_call_rst")]
+    private static extern SampleStruct get_simple_struct();
 
     private static void Main(string[] args)
     {
@@ -13,5 +23,9 @@ public static class RustTest
         var addedNumbers = add_numbers(10, 5);
         Console.WriteLine($"Returned value: {addedNumbers}");
         Console.ReadLine();
+
+        var simple_struct = get_simple_struct();
+        Console.WriteLine(simple_struct.field_one);
+        Console.WriteLine(simple_struct.field_two);
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,3 +7,17 @@ pub extern fn add_numbers(number1: i32, number2: i32) -> i32 {
     println!("Hello from rust!");
     number1 + number2
 }
+
+#[repr(C)]
+pub struct SampleStruct {
+    pub field_one: i16,
+    pub field_two: i32,
+}
+
+#[no_mangle]
+pub extern fn get_simple_struct() -> SampleStruct {
+    SampleStruct {
+        field_one: 1,
+        field_two: 2
+    }
+}
